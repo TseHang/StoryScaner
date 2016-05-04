94d6ff65815cbab3a82e38b5b7d6b41c373965ad
TseHang <a54383813@gmail.com>
2016-05-04 16:52:58 +0800

    add dblclick

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,12 +1,13 @@
 
-$('#logo').delay(2000).animate({"opacity":0},400);
-$('#storyscaner').delay(1500).animate({"opacity":0},400 , function(){
+$('#logo').delay(2000).animate({"opacity":0},300 , function(){
   $('body').removeClass("loading");
 });
+$('#storyscaner').delay(1800).animate({"opacity":0},400);
 
 
 $(document).ready(function (){
   var introSwiper = new Swiper ('.intro-container', {
+    preloadImages:true,
     wrapperClass : "intro-wrapper",
     direction: 'horizontal',
     //loop: true,
