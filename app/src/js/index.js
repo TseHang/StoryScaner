87c494fe369c6c42aa6c0b614edf95c7fc90071a
TseHang <a54383813@gmail.com>
2016-05-04 09:23:30 +0800

    fix delay time

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,6 +1,6 @@
 //$('#logo_wrapper').animo({animation: "spinner", duration: 1 , iterate: 3});/*
 
-$('#logo_wrapper').delay(500).animate({ width:"30%" },1000)
+$('#logo_wrapper').delay(1000).animate({ width:"30%" },1000)
   .animate({ width:0, top: "20%" },400 , function(){
     $('body').removeClass("loading");
   });
