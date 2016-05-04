dd54d17afad0316339607dd3d59067e53da09779
TseHang <a54383813@gmail.com>
2016-05-04 12:12:44 +0800

    fix img

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,10 +1,9 @@
 //$('#logo_wrapper').animo({animation: "spinner", duration: 1 , iterate: 3});/*
-
-$('#logo_wrapper').delay(1000).animate({ width:"30%" },1000)
-  .animate({ width:0, top: "20%" },400 , function(){
+/*
+$('#logo_wrapper').delay(1500).animate({ width:0, top: "20%" },500 , function(){
     $('body').removeClass("loading");
   });
-
+*/
 
 $(document).ready(function (){
   var introSwiper = new Swiper ('.intro-container', {
