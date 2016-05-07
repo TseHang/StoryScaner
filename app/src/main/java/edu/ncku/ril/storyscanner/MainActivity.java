67efb606faba0a87328d4c8a480b32eb8a327334
Ril Jian <rilislearning@gmail.com>
2016-05-07 09:48:58 +0800

    Display by WebView

---
+++ app/src/main/java/edu/ncku/ril/storyscanner/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.ncku.ril.storyscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://tsehang.github.io/StoryScaner/app");
+    }
+
+}
