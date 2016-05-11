03d53b68b8ee49210d52cae72bbde82fc8b5b2fa
TseHang <a54383813@gmail.com>
2016-05-11 15:35:05 +0800

    test camera

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -28,9 +28,11 @@ $(document).ready(function (){
     introSwiper.removeSlide(0); 
   })
 
-  $('.intro2-content').click(function(){
-  	console.log('11');
-  	console.log($(this));
+  $('.intro2-content').click(function(e){
+  	//移動
+  	e.preventDefault(); 
+  	goToByScroll(this.id);  
+
   	if (intro2ContentToggle == 0){
   		$(this).addClass('intro2-content-show');
 	  	$(this).children('p.intro2-detail').animate({
@@ -39,6 +41,7 @@ $(document).ready(function (){
 	  	},400)
 
 	  	intro2ContentToggle =1 ;
+	  	console.log('展開囉');
   	}
   	else if (intro2ContentToggle == 1 ){
   		$(this).removeClass('intro2-content-show');
@@ -48,7 +51,91 @@ $(document).ready(function (){
 	  	});
 
 	  	intro2ContentToggle =0 ;
+	  	console.log('收縮囉');
   	}
   })
 
+  $('.intro2-button').click(function(){
+		console.log("111");
+		introSwiper.slideNext();
+
+	})
+});
+
+function goToByScroll(id){
+  // Scroll
+  $('html,body').animate({
+    scrollTop: $("#"+id).offset().top},400);
+}
+
+
+////// Web RTC 技術 /////////
+/*
+function hasGetUserMedia() {
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported in your browser');
+}
+
+var errorCallback = function(e) {
+  console.log('Reeeejected!', e);
+};
+
+navigator.getUserMedia  = navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia;
+
+// Not showing vendor prefixes.
+navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+  var video = document.querySelector('video');
+  video.src = window.URL.createObjectURL(localMediaStream);
+
+  // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+  // See crbug.com/110938.
+  video.onloadedmetadata = function(e) {
+    // Ready to go. Do some stuff.
+  };
+}, errorCallback);
+*/
+
+// Put event listeners into place
+window.addEventListener("DOMContentLoaded", function() {
+	// Grab elements, create settings, etc.
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		video = document.getElementById("video"),
+		videoObj = { "video": true },
+		errBack = function(error) {
+			console.log("Video capture error: ", error.code); 
+		};
+
+	// Put video listeners into place
+	if(navigator.getUserMedia) { // Standard
+		navigator.getUserMedia(videoObj, function(stream) {
+			video.src = stream;
+			video.play();
+		}, errBack);
+	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+		navigator.webkitGetUserMedia(videoObj, function(stream){
+			video.src = window.webkitURL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+	else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+		navigator.mozGetUserMedia(videoObj, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+	}
+}, false);
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
 });
