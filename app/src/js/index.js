0f9141972214b328f2fc485ce11759155118d3a2
TseHang <a54383813@gmail.com>
2016-05-11 10:50:23 +0800

    app2

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,32 +1,54 @@
 $(window).load(function(){
   console.log("都載入完囉");
-  $('#logo').delay(2000).animate({"opacity":0},300 , function(){
-    $('body').removeClass("loading");
-    console.log(12);
+  $('#modal img').delay(2000).animate({"opacity":0}, 500 , function(){
+    $('#modal').css("display","none");
+    console.log("loading結束了");
   });
-  $('#storyscaner').delay(1800).animate({"opacity":0},400);
 })
 
+var intro2ContentToggle = 0;
+
 $(document).ready(function (){
+
   console.log("Dom Tree 建好了");
+  
   var introSwiper = new Swiper ('.intro-container', {
-    preloadImages:true,
-    wrapperClass : "intro-wrapper",
     direction: 'horizontal',
     //loop: true,
-    speed: 400,
-  });
+  }) ;
   introSwiper.lockSwipeToPrev();
   introSwiper.lockSwipeToNext();
 
-  $('.swiper-slide').dblclick(function(){
+  $('a#okbtn').click(function(){
+  	introSwiper.unlockSwipeToNext();
     introSwiper.unlockSwipeToPrev();
-    introSwiper.unlockSwipeToNext();
+    introSwiper.slideNext();
+
+    //Bug -- 應該要先滑動，再移除（時間點衝到了！）
+    introSwiper.removeSlide(0); 
   })
 
-  $('div#border-flex a').click(function(){
-    introSwiper.unlockSwipeToNext();
-    introSwiper.slideNext();
-    introSwiper.lockSwipeToNext();
+  $('.intro2-content').click(function(){
+  	console.log('11');
+  	console.log($(this));
+  	if (intro2ContentToggle == 0){
+  		$(this).addClass('intro2-content-show');
+	  	$(this).children('p.intro2-detail').animate({
+	  		'margin-top':'1em',
+	  		'opacity':'1'
+	  	},400)
+
+	  	intro2ContentToggle =1 ;
+  	}
+  	else if (intro2ContentToggle == 1 ){
+  		$(this).removeClass('intro2-content-show');
+	  	$(this).children('p.intro2-detail').css({
+	  		'margin-top':'auto',
+	  		'opacity':'0'
+	  	});
+
+	  	intro2ContentToggle =0 ;
+  	}
   })
+
 });
